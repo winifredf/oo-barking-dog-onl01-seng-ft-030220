54abcf54b90ca729ce6c43f0a81d91ee8cf4c7b6
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-23 01:23:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
+class Dog
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+  
+  def name
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+
+
 # Your code goes here!
